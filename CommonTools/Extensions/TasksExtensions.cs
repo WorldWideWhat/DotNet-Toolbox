be4355a959d58c87864be6e373a3928c8433fc64
Lars Hederidder <coder@worldwidewhat.dk>
2022-02-12 19:28:20 +0100

    Initial commit

---
+++ CommonTools/Extensions/TasksExtensions.cs
@@ -0,0 +1,14 @@
+﻿public static partial class TasksExtensions
+{
+    public static async Task<T> Timeout<T>(this Task<T> task, int time)
+    {
+        var cts = new CancellationTokenSource(time);
+        var tcs = new TaskCompletionSource<bool>();
+        using (cts.Token.Register(s => ((TaskCompletionSource<bool>)s).TrySetResult(true), tcs))
+        {
+            if (task != await Task.WhenAny(task, tcs.Task))
+                throw new System.OperationCanceledException();
+        }
+        return await task;
+    }
+}
